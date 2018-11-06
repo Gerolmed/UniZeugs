1ba03107b9f7133dd9e91dfba71315dfaf370cac
Gerolmed <werwolf420@yahoo.de>
2018-11-06 15:30:01 +0100

    Added useful comments

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,3 +1,12 @@
+/**
+ * The amazingly fun coop game is now available to your local klappkroutsch TM.
+ * Enjoy milliseconds of fun. (Probably because of a slow computer and raging teammates)
+ * The competitive mode is extraordinary and tends to end with a tie. (Using the Trueskill-Algorithm (not!!))
+ * Family Game Award 2020.
+ * "So much fun" - nobody
+ * "10/10" - GameStar
+ * "No my live seems boring compared to this game" - still nobody
+ */
 public class FizzBuzz {
 
     public void play(int goal) {
