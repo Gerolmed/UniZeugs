bc3496e4a704d71b55f9b8f5497477fe4631ac8f
Gerolmed <werwolf420@yahoo.de>
2018-11-27 12:46:58 +0100

    Rudimentary setup v2 Its not working though

---
+++ src/de/shop/storage/DVDPlayer.java
@@ -0,0 +1,44 @@
+package de.shop.storage;
+
+public class DVDPlayer extends Product{
+
+    private String currentDVD;
+    private boolean blueRay;
+
+    public DVDPlayer(String NAME, int PRODUCT_NUMBER, double priceBuy, double priceSell, int stock, String currentDVD, boolean blueRay) {
+        super(NAME, PRODUCT_NUMBER, priceBuy, priceSell, stock);
+        this.currentDVD = currentDVD;
+        this.blueRay = blueRay;
+    }
+
+    public DVDPlayer(String NAME, int PRODUCT_NUMBER, double priceBuy, double priceSell, String currentDVD, boolean blueRay) {
+        super(NAME, PRODUCT_NUMBER, priceBuy, priceSell);
+        this.currentDVD = currentDVD;
+        this.blueRay = blueRay;
+    }
+
+
+    public void changeDVD(String newDVD) {
+        this.currentDVD = newDVD;
+    }
+
+    public boolean getBlueRay() {
+        return blueRay;
+    }
+
+    public String getCurrentDVD() {
+        return currentDVD;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if(!(obj instanceof DVDPlayer))
+            return false;
+
+        DVDPlayer pr = (DVDPlayer) obj;
+
+        return pr.currentDVD.equals(this.getCurrentDVD()) &&
+                pr.getBlueRay() == this.getBlueRay();
+    }
+}
--- src/de/shop/storage/Product.java
+++ src/de/shop/storage/Product.java
@@ -2,6 +2,57 @@ package de.shop.storage;
 
 public class Product {
 
+    public final String NAME;
+    private final int PRODUCT_NUMBER;
+
+    private double priceBuy, priceSell;
+    private int stock;
+
+    public Product(String NAME, int PRODUCT_NUMBER, double priceBuy, double priceSell, int stock) {
+        this.NAME = NAME;
+        this.PRODUCT_NUMBER = PRODUCT_NUMBER;
+        this.priceBuy = priceBuy;
+        this.priceSell = priceSell;
+        this.stock = stock;
+    }
+
+    public Product(String NAME, int PRODUCT_NUMBER, double priceBuy, double priceSell) {
+        this.NAME = NAME;
+        this.PRODUCT_NUMBER = PRODUCT_NUMBER;
+        this.priceBuy = priceBuy;
+        this.priceSell = priceSell;
+    }
 
 
+    public int getPRODUCT_NUMBER() {
+        return PRODUCT_NUMBER;
+    }
+
+    public double getPriceBuy() {
+        return priceBuy;
+    }
+
+    public double getPriceSell() {
+        return priceSell;
+    }
+
+    public void priceChange(double change) {
+        this.priceSell = change;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if(!(obj instanceof Product))
+            return false;
+
+        Product pr = (Product) obj;
+
+        return pr.NAME.equals(this.NAME);
+    }
+
+    @Override
+    public String toString() {
+        return "A "+NAME+" costs $"+getPriceBuy()+". There are "+stock+" left in storage.";
+    }
 }
