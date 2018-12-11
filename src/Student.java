2d011ee1aef8fd85f16abf9df6505e897ddbacfb
Gerolmed <werwolf420@yahoo.de>
2018-12-11 10:56:40 +0100

    A not so awesome university

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student extends Person implements Mailable {
+
+    private int matriculationNumber;
+
+    public Student(String firstName, String surName, String birthday, int matriculationNumber) {
+        super(firstName, surName, birthday);
+        this.matriculationNumber = matriculationNumber;
+    }
+
+    @Override
+    public String getEmailAddress() {
+        return String.format("%s.%s@stud.uni-fictitious.de", getFirstName(), getSurName());
+    }
+
+    @Override
+    public int getIdentifier() {
+        return matriculationNumber;
+    }
+}
